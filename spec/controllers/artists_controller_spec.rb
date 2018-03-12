905321d7d3dd364f96f5aa0dc95c378af4432ef3
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-12 18:01:07 +0000

    automatically backed up by learn

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ArtistsController do
     it "redirects when access is turned off" do
       p = Preference.create(allow_create_artists: false)
       get :new
-      expect(response).to redirect_to artists_path 
+      expect(response).to redirect_to artists_path
     end
   end
 
