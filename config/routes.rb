0a6a9acd945b2b1ea1c492e1f3c172b33606ebae
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-12 17:32:26 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :preferences
   end
-  
+
   resources :artists do
     resources :songs, only: [:index, :show]
   end
-  
+
   resources :songs
 
 end
